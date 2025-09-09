18d1252e3a94f6dc93c4db1aa0964b2cc60da5fd
Ekta <ektaarora3501@gmail.com>
2025-09-09 17:58:15 +0530

    updating readme

--- src/app.js
+++ src/app.js
@@ -37,15 +37,14 @@ app.use("/api/v1", indexRoutes);
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
- 
+
   socket.on('alert', (data) => {
     io.emit('newAlert', JSON.stringify(data));
 
   });
 
-    socket.on('count', (data) => {
+  socket.on('count', (data) => {
     io.emit('countUpdate', JSON.stringify(data));
-
   });
 
   socket.on('disconnect', () => {
